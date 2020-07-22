dfc4aed3644fdaf6f53bbb3fd9e14496ec360fd6
Jared Jessup <jessupjs@gmail.com>
2020-07-22 13:05:18 -0400

    Checking for bugs w Robert

--- src/lensing.js
+++ src/lensing.js
@@ -370,7 +370,7 @@ export default class Lensing {
 
                     // Lens border / stroke
                     this.overlay.context.strokeStyle = `white`;
-                    this.overlay.context.strokeWidth = `2px`;
+                    this.overlay.context.lineWidth = 1;
                     this.overlay.context.beginPath();
                     if (this.configs.shape === 'circle') {
                         this.overlay.context.arc(this.configs.rad, this.configs.rad, this.configs.rad - 1, 0, Math.PI * 2);
--- src/viewfinder.js
+++ src/viewfinder.js
@@ -18,28 +18,33 @@ export default class Viewfinder {
         g: null,
         radialG: null,
         svg: null,
+        boxG: null
     };
 
     // Configs
     configs = {
+        boxW: 150,
+        boxH: 50,
         deg: 0,
-        extend: 50,
+        extend: 200,
         gMargin: {top: 0, left: 0, right: 0, bottom: 0},
         gH: 0,
+        gW: 0,
         h: 0,
         r: 0,
         rPointer: 0,
-        rPointerExt: 10,
+        rPointerExt: 20,
         w: 0,
-        gW: 0
     };
 
-    // Tools
+    // Tools - TODO add boxScale to trace perimeter
     tools = {
         coordScale: d3.scaleLinear()
             .domain([0, 360])
             .range([Math.PI, -Math.PI]),
-        lineMaker: d3.line()
+        lineMaker: d3.line(),
+        xScale: d3.scaleLinear(),
+        yScale: d3.scaleLinear()
     }
 
     /*
@@ -78,6 +83,15 @@ export default class Viewfinder {
             .attr('fill', 'none')
             .attr('stroke', 'rgba(255, 255, 255, 1)')
             .style('transform', 'translate3d(0, 0, 0)');
+        vis.els.boxG = vis.els.dataPointG.append('g')
+            .attr('class', 'viewfinder_box_g')
+            .style('transform', 'translate3d(0, 0, 0)');
+        vis.els.boxG.append('rect')
+            .attr('width', vis.configs.boxW)
+            .attr('height', vis.configs.boxH)
+            .attr('fill', 'none')
+            .attr('stroke', 'rgba(255, 255, 255, 1)')
+            .attr('stroke-width', '1px');
 
     }
 
@@ -105,6 +119,14 @@ export default class Viewfinder {
             const y = this.lensing.configs.pos[1] / vis.lensing.configs.pxRatio - this.lensing.viewer.canvas.clientHeight / 2;
             vis.deg = Math.atan2(y, x) * (180 / Math.PI);
         }
+
+        // Update tools
+        vis.tools.xScale
+            .domain([-vis.configs.rPointer, vis.configs.rPointer])
+            .range([-vis.configs.boxW, 0]);
+        vis.tools.yScale
+            .domain([-vis.configs.rPointer, vis.configs.rPointer])
+            .range([-vis.configs.boxH, 0]);
 
         // Render
         vis.render()
@@ -135,10 +157,15 @@ export default class Viewfinder {
 
                     // Pointer coords
                     const pCoords = getCoords(vis.configs.rPointer, vis.deg - 90);
+                    const addX = Math.round(vis.tools.xScale(pCoords[0]));
+                    const addY = Math.round(vis.tools.yScale(pCoords[1]));
 
                     // Update path
                     g.select('path')
                         .attr('d', vis.tools.lineMaker([[0, 0], pCoords]));
+
+                    // Update boxG pos
+                    vis.els.boxG.style('transform', `translate(${pCoords[0] + addX}px, ${pCoords[1] + addY}px)`);
                 });
         }
 
